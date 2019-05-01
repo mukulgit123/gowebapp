cc8e2ae9a1ff6e7387652af0027dabef850f76d8
Mukul Garg <mukul.garg@tothenew.com>
2019-05-01 16:00:55 +0530

    Adding the new logic.

--- src/github.com/mukulgit123/gowebapp/main.go
+++ src/github.com/mukulgit123/gowebapp/main.go
@@ -8,25 +8,32 @@ package main
 import (
 	// "fmt" has methods for formatted I/O operations (like printing to the console)
 	"fmt"
-	// The "net/http" library has methods to implement HTTP clients and servers
+	
+	"strings"
+
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	// The "HandleFunc" method accepts a path and a function as arguments
 	// (Yes, we can pass functions as arguments, and even trat them like variables in Go)
 	// However, the handler function has to have the appropriate signature (as described by the "handler" function below)
-	http.HandleFunc("/", handler)
-
-	// After defining our server, we finally "listen and serve" on port 8080
-	// The second argument is the handler, which we will come to later on, but for now it is left as nil,
-	// and the handler defined above (in "HandleFunc") is used
-	http.ListenAndServe(":8080", nil)
+	r :=  mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+	r.HandleFunc("/bye", handler).Methods("GET")
+	http.ListenAndServe(":8080", r)
 }
 
 // "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
 // as the arguments.
 func handler(w http.ResponseWriter, r *http.Request) {
 	// For this case, we will always pipe "Hello World" into the response writer
-	fmt.Fprintf(w, "Hello World!")
+	//fmt.Fprintf(w,r)
+	if strings.Contains(string(r.URL.Path),"bye")	{
+	fmt.Fprintf(w, "Good Bye!\n")
+	} else	{
+	fmt.Fprintf(w,"Hello World!\n")
+	}
 }
