78ec2239a373ec209cd46ffa01ccacb02fc2cf15
Mukul Garg <mukul.garg@ymail.com>
2019-05-08 11:04:07 +0530

    Removed unnecessary comments from the code

--- src/github.com/mukulgit123/gowebapp/main.go
+++ src/github.com/mukulgit123/gowebapp/main.go
@@ -1,12 +1,7 @@
-// This is the name of our package
-// Everything with this package name can see everything
-// else inside the same package, regardless of the file they are in
 package main
 
-// These are the libraries we are going to use
-// Both "fmt" and "net" are part of the Go standard library
+
 import (
-	// "fmt" has methods for formatted I/O operations (like printing to the console)
 	"fmt"
 	
 	"strings"
@@ -17,17 +12,12 @@ import (
 )
 
 func main() {
-	// The "HandleFunc" method accepts a path and a function as arguments
-	// (Yes, we can pass functions as arguments, and even trat them like variables in Go)
-	// However, the handler function has to have the appropriate signature (as described by the "handler" function below)
 	r :=  mux.NewRouter()
 	r.HandleFunc("/hello", handler).Methods("GET")
 	r.HandleFunc("/bye", handler).Methods("GET")
 	http.ListenAndServe(":8080", r)
 }
 
-// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
-// as the arguments.
 func handler(w http.ResponseWriter, r *http.Request) {
 	// For this case, we will always pipe "Hello World" into the response writer
 	//fmt.Fprintf(w,r)
